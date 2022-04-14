bc925fe700dd6b9edbaff45193ff3861094a7f5b
jose.raul.navarro.avila27@uabc.edu.mx <jose.raul.navarro.avila27@uabc.edu.mx>
2022-04-13 21:33:14 -0700

    Agregar archivos de proyecto.

---
+++ Readline/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nombre;
+
+            //Se pregunta el nombre del usuario 
+            Console.WriteLine("¿Como se llama?");
+            nombre = Console.ReadLine(); //Se guaeda su respuesta  en la variable 
+
+            Console.WriteLine("Hola " + nombre + " ¿Quiere ver el menu?");
+            Console.ReadKey();
+
+        }
+    }
+}
